c092b9051d7fd90121c600ff4025f903ae0b8bac
Franco <francolopezpintos@gmail.com>
2022-05-26 23:08:53 -0300

    ok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.POST || 3030;
+const port = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname, "./public")));
 
