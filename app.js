3e863034e480d39063603926f140e71b92a186c4
Franco <francolopezpintos@gmail.com>
2022-05-26 22:41:34 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.POST || 3030;
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(3030, () =>
+app.listen(port, () =>
     console.log("Levantando un servidor con Express"));
 
 app.get("/", (req, res) => {
